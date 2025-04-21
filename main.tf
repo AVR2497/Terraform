73f564042910f3dd44112109a6519f89508772bc
AVR2497 <antonioval2497@gmail.com>
2025-04-21 09:00:51 +0200

    Añadir configuraciones inicial de Terraform con Localstack

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  s3_use_path_style           = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+    s3 = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b141545c95a79" # AMI ficticia
+  instance_type = "t2.small"
+}
